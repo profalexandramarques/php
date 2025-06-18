9a83b3f52bf01202b1da3190f110a9fdc0e78e2a
Alexandra Marques <prof.alexandra.marques@gmail.com>
2025-06-18 10:25:03 -0300

    Add files via upload

--- loja/index.php
+++ loja/index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,13 +8,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
      <form method="post">
          <h1>Produtos</h1>
          <label for="fid">Código:</label><br>
-          <input type="text" id="id_codigo" name="id_codigo" value=""><br>
+          <input type="text" id="codigo" name="codigo" value=""><br>
           <label for="fnome">Nome:</label><br>
           <input type="text" id="nome" name="nome" value=""><br>
           <label for="fvalor">Valor:</valor><br>
           <input type="number" id="valor" name="valor" step = '0.01' value=""><br>
           <input type="checkbox" id="ativo" name="ativo" value="A" checked> <label for="lativo">Ativo</label><br> <br>
-           <input type="submit" value="Cadastrar">
+          <input type="submit" value="Cadastrar">
      </form>  
 
         <?php
@@ -26,14 +22,15 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             $caminho_banco = 'bdloja.db';
             $pdo = new PDO("sqlite:" . $caminho_banco);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    
             //echo "Conexão com o banco de dados SQLite realizada com sucesso!<br>";
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {   
-                $codigo = $POST['codigo'];
-                $nome  = $POST['nome'];
-                $valor = $POST['valor'];
-                $ativo = $POST['ativo'];
+            if ($_SERVER["REQUEST_METHOD"] == "POST"){   
+                $codigo = $_POST['codigo'];
+                $nome  = $_POST['nome'];
+                $valor = $_POST['valor'];
+                $ativo = $_POST['ativo'];
                 
-                $sql_inserir = "INSERT INTO produtos (id_produto, nome, valor, ativo) VALUES(:codigo, :nome, :valor,:ativo);";
+                $sql_inserir = "INSERT INTO produtos (id_produto, nome, valor, ativo) VALUES(:codigo, :nome, :valor, :ativo);";
                 // Prepara a query SQL
                 $stmt = $pdo->prepare($sql_inserir); 
                 $stmt->bindParam(':codigo', $codigo); 
@@ -42,9 +39,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                 $stmt->bindParam(':ativo', $ativo); 
                 $stmt->execute(); // Executa a query 
                 echo "Produto cadastrado com sucesso!";
-
-                
-            }
+            }   
+            
             //Listar todos os produtos
            $sql = "SELECT * FROM produtos ORDER BY nome";
            $stmt = $pdo->query($sql); //Prepara a consulta
@@ -60,10 +56,10 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
              echo "<td>" . $produto['valor'] . "</td>";
              echo "<td>" . $produto['ativo'] . "</td>";
              echo "</tr>";
-          
+           }
           echo "</table>";
-          $pdo = null;
-            }
+          //$pdo = null;
+            
         ?>
     </body>
 </html>
